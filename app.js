0fc4656fc52479ffb00fc9e9901c35d74ae88341
timanity <83787104+timanity@users.noreply.github.com>
2023-06-30 02:32:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,80 +1,68 @@
 const express = require('express');
 const admin = require('firebase-admin');
-const app = express();
-
 const serviceAccount = require("./dnvrml-firebase-adminsdk-4324d-ed280e5d01.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
 
-const firestore = admin.firestore();
+const db = admin.firestore();
+const app = express();
 
 app.set('port', (process.env.PORT || 3001));
 
 app.get('*', (req, res) => {
-    res.send('Madden Companion Exporter');
+  res.send('Madden Companion Exporter');
 });
 
-app.post('/:username/:platform/:leagueId/leagueteams', async (req, res) => {
-    let body = '';
-    req.on('data', chunk => {
-        body += chunk.toString();
-    });
-    req.on('end', async () => {
-        const { leagueTeamInfoList: teams } = JSON.parse(body);
-        const {params: { username, leagueId }} = req;
+app.post('/:username/:platform/:leagueId/standings', (req, res) => {
+  let body = '';
+  req.on('data', chunk => {
+    body += chunk.toString();
+  });
+  req.on('end', async () => {
+    console.log(body); // Added debug log
+    const { teamStandingInfoList: teams } = JSON.parse(body);
+    const {params: { username, leagueId }} = req;
 
-        for (let team of teams) {
-            const teamRef = firestore.doc(`data/${username}/${leagueId}/teams/${team.teamId}`);
-            await teamRef.set(team);
-        }
+    for(let team of teams) {
+      const teamRef = db.collection(`data/${username}/${leagueId}/teams`).doc(`${team.teamId}`);
+      await teamRef.set(team)
+        .then(() => console.log(`Wrote team ${team.teamId} to Firestore`)) // Log successful writes
+        .catch(err => console.error(`Error writing team ${team.teamId} to Firestore: ${err}`)); // Log errors
+    }
 
-        res.sendStatus(200);
-    });
-});
-
-app.post('/:username/:platform/:leagueId/standings', async (req, res) => {
-    let body = '';
-    req.on('data', chunk => {
-        body += chunk.toString();
-    });
-    req.on('end', async () => {
-        const { teamStandingInfoList: teams } = JSON.parse(body);
-        const {params: { username, leagueId }} = req;
-
-        for(let team of teams) {
-            const teamRef = firestore.doc(`data/${username}/${leagueId}/teams/${team.teamId}`);
-            await teamRef.set(team);
-        }
-
-        res.sendStatus(200);
-    });
-});
-
-app.post('/:username/:platform/:leagueId/week/:weekType/:weekNumber/:dataType', async (req, res) => {
-    let body = '';
-    req.on('data', chunk => {
-        body += chunk.toString();
-    });
-    req.on('end', async () => {
-        const property = `player${capitalizeFirstLetter(req.params.dataType)}StatInfoList`;
-        const items = JSON.parse(body)[property];
-        const {params: { username, leagueId, weekType, weekNumber }} = req;
-
-        for(let item of items) {
-            const itemRef = firestore.doc(`data/${username}/${leagueId}/${weekType}/${weekNumber}/teams/${item.teamId}/player-stats/${item.rosterId}`);
-            await itemRef.set(item);
-        }
-
-        res.sendStatus(200);
-    });
-});
-
-app.listen(app.get('port'), function () {
-    console.log('Server started on port ' + app.get('port'));
+    res.sendStatus(200);
+  });
 });
 
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+app.post('/:username/:platform/:leagueId/week/:weekType/:weekNumber/:dataType', (req, res) => {
+  let body = '';
+  req.on('data', chunk => {
+    body += chunk.toString();
+  });
+  req.on('end', async () => {
+    console.log(body); // Added debug log
+    const {params: { username, leagueId, weekType, weekNumber, dataType }} = req;
+    const statsPath = `data/${username}/${leagueId}/stats`;
+    const property = `player${capitalizeFirstLetter(dataType)}StatInfoList`;
+    const stats = JSON.parse(body)[property];
+
+    for(let stat of stats) {
+      const weekRef = db.collection(`${statsPath}/${weekType}/${weekNumber}/${stat.teamId}/player-stats`).doc(`${stat.rosterId}`);
+      await weekRef.set(stat)
+        .then(() => console.log(`Wrote stat for player ${stat.rosterId} to Firestore`)) // Log successful writes
+        .catch(err => console.error(`Error writing stat for player ${stat.rosterId} to Firestore: ${err}`)); // Log errors
+    }
+
+    res.sendStatus(200);
+  });
+});
+
+app.listen(app.get('port'), function () {
+  console.log('Server started on port ' + app.get('port'));
+});
